eded13e36ab2a2e7ff5864f3e1e3d385da2102e2
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2026-01-09 21:34:37 +0200

    benefits

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,28 @@
+import Benefit from "@/components/Benefit/Benefit";
 import css from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={css.container}>
+    <div>
       {" "}
-      <div className={css.box}>
-        <h1 className={css.title}>
-          Unlock your potential with the best language tutors
-        </h1>
-        <p className={css.description}>
-          Embark on an Exciting Language Journey with Expert Language Tutors:
-          Elevate your language proficiency to new heights by connecting with
-          highly qualified and experienced tutors.
-        </p>
-        <button className={css.bttn}>Get started</button>
+      <div className={css.container}>
+        {" "}
+        <div className={css.box}>
+          <h1 className={css.title}>
+            Unlock your potential with the best language tutors
+          </h1>
+          <p className={css.description}>
+            Embark on an Exciting Language Journey with Expert Language Tutors:
+            Elevate your language proficiency to new heights by connecting with
+            highly qualified and experienced tutors.
+          </p>
+          <button className={css.bttn}>Get started</button>
+        </div>
+        <div className={css.img}></div>
       </div>
-      <div className={css.img}></div>
+      <div>
+        <Benefit />
+      </div>
     </div>
   );
 }
---
+++ components/Benefit/Benefit.tsx
@@ -0,0 +1,26 @@
+import css from "./Benefit.module.css";
+
+const Benefit = () => {
+  return (
+    <div className={css.block}>
+      <div className={css.box}>
+        <p className={css.number}>32,000 +</p>
+        <p className={css.text}>Experienced tutors</p>
+      </div>
+      <div className={css.box}>
+        <p className={css.number}>300,000 +</p>
+        <p className={css.text}>5-star tutor reviews</p>
+      </div>
+      <div className={css.box}>
+        <p className={css.number}>120 +</p>
+        <p className={css.text}>Subjects taught</p>
+      </div>
+      <div className={css.box}>
+        <p className={css.number}>200 +</p>
+        <p className={css.text}>Tutor nationalities</p>
+      </div>
+    </div>
+  );
+};
+
+export default Benefit;
