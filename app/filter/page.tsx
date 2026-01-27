da822920b8ad16c76e2481b3682c7336ca94d36d
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2026-01-27 22:47:47 +0200

    cards

--- app/filter/page.tsx
+++ app/filter/page.tsx
@@ -1,10 +1,12 @@
 import TeachersList from "./TeachersList";
 import css from "./TeachersLict.module.css";
+import Cards from "@/components/Cards/Cards";
 
 const page = () => {
   return (
     <div className={css.container}>
       <TeachersList />
+      <Cards />
     </div>
   );
 };
